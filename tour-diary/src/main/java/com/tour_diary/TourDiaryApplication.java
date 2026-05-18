0c79c06b2da3fcb58e2b483523bc5f7dcae92c81
KHS <gudtnslwkd@naver.com>
2026-05-18 23:58:47 +0900

    feat: First PJT Commit

---
+++ tour-diary/src/main/java/com/tour_diary/TourDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.tour_diary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourDiaryApplication.class, args);
+	}
+
+}
